e557f2f0073d1431a32a0e4f8b5a096e01fd3e1a
Brenda J. Freire da Costa <brendajoycefreire@gmail.com>
2024-11-18 12:48:45 -0300

    Criando uma tupla para armazenar a variável 'endereco'

--- PrimeiroPrograma.py
+++ PrimeiroPrograma.py
@@ -8,3 +8,8 @@ hobbies1 = hobbies.split(',')
 # que provavelmente contém uma string de palavras ou frases separadas por vírgulas, 
 # e a "quebrando" em uma lista de substrings, utilizando a vírgula como delimitador.
 # O método .split(',') divide a string sempre que encontra uma vírgula, gerando elementos separados na lista hobbies1.
+
+endereco_rua = input('Digite o nome da sua rua: ')
+endereco_numero = int(input('Digite o número da sua casa: '))
+endereco_cidade = input('Digite a sua cidade: ')
+endereco = (endereco_rua, endereco_numero, endereco_cidade)
