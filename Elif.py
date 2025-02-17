7a322ba2dd9655875b7aeae7731d678606ca45d4
Brenda Freire <brendajoycefreire@gmail.com>
2025-02-17 15:44:24 -0300

    utilizando o elif

---
+++ Elif.py
@@ -0,0 +1,11 @@
+# O elif é uma palavra-chave em Python que significa "senão, se" e pode ser considerado 
+# uma união do else com um if. Ela é usada em conjunto com a palavra-chave if para formar uma estrutura condicional encadeada.
+
+# if condição1:
+    # faça algo
+# elif condição2:
+    # faça outra coisa
+# elif condição3:
+    # faça mais alguma coisa
+# else:
+    # faça algo diferente
